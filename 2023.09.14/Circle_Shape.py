23b107dc6c29622caced8d979731e2cf87845b7e
juyeon_Prestige16 <zooy2on01@gmail.com>
2023-09-14 11:23:21 +0900

    입력 받은 도형 그리기 : 사각형 추가

--- 2023.09.14/Circle_Shape.py
+++ 2023.09.14/Circle_Shape.py
@@ -10,5 +10,17 @@ elif shape == 'triangle' :
     turtle.forward(50); turtle.left(120)
     turtle.forward(50); turtle.left(50)
     turtle.forward(50)
+elif shape == 'square' :
+    turtle.reset()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
 else:
     print('Unknown shape')
