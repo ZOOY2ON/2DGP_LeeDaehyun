acaefbc6ae26e1f59c8519b0d63add512cf2743a
juyeon_Prestige16 <zooy2on01@gmail.com>
2023-09-14 09:53:45 +0900

    입력 받은 도형 그리기 : 원,삼각형

---
+++ 2023.09.14/Circle_Shape.py
@@ -0,0 +1,14 @@
+import turtle
+
+shape = input("Enter Shape")
+
+if shape == 'circle' :
+    turtle.reset()
+    turtle.circle(50)
+elif shape == 'triangle' :
+    turtle.reset()
+    turtle.forward(50); turtle.left(120)
+    turtle.forward(50); turtle.left(50)
+    turtle.forward(50)
+else:
+    print('Unknown shape')
